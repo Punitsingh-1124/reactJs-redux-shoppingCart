bc22c5f72ad5f3555cb61f982cbf6047e81d8125
Punit Singh <punitsingh2413@gmail.com>
2025-05-06 18:02:52 +0530

    Update Cart.jsx

--- src/stores/Cart.jsx
+++ src/stores/Cart.jsx
@@ -1,41 +1,85 @@
+//code : 1
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
-    items: localStorage.getItem("carts") ? JSON.parse(localStorage.getItem("carts")) : [],
+    items: [],       // No localStorage
     stateTab: false
-}
+};
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
         addToCart(state, action) {
             const { productId, quantity } = action.payload;
-            const indexProductId = (state.items).findIndex((item => item.productId === productId));
+            const indexProductId = state.items.findIndex(item => item.productId === productId);
             if (indexProductId >= 0) {
                 state.items[indexProductId].quantity += quantity;
             } else {
                 state.items.push({ productId, quantity });
             }
-            localStorage.setItem("carts", JSON.stringify(state.items));
         },
         changeQuantity(state, action) {
             const { productId, quantity } = action.payload;
-            const indexProductId = (state.items).findIndex(item => item.productId === productId)
+            const indexProductId = state.items.findIndex(item => item.productId === productId);
             if (quantity > 0) {
                 state.items[indexProductId].quantity = quantity;
             } else {
-                // delete state.items[indexProductId];
-                state.items = (state.items).filter(item => item.productId !== productId)
+                state.items = state.items.filter(item => item.productId !== productId);
             }
         },
-
         toggleStatusTab(state) {
-            if (state.stateTab === false) {
-                state.stateTab = true;
-            } else {
-                state.stateTab = false;
-            }
+            state.stateTab = !state.stateTab;
         }
     }
-})
+});
+
 export const { addToCart, changeQuantity, toggleStatusTab } = cartSlice.actions;
 export default cartSlice.reducer;
+
+
+
+
+//code : 2
+//Use this when you need to store add data in browser and if any promblem you face than comment this all code and use code no. 1
+// import { createSlice } from "@reduxjs/toolkit";
+// const initialState = {
+//     items: localStorage.getItem("carts") ? JSON.parse(localStorage.getItem("carts")) : [],
+//     stateTab: false
+// }
+// const cartSlice = createSlice({
+//     name: 'cart',
+//     initialState,
+//     reducers: {
+//         addToCart(state, action) {
+//             const { productId, quantity } = action.payload;
+//             const indexProductId = (state.items).findIndex((item => item.productId === productId));
+//             if (indexProductId >= 0) {
+//                 state.items[indexProductId].quantity += quantity;
+//             } else {
+//                 state.items.push({ productId, quantity });
+//             }
+//             localStorage.setItem("carts", JSON.stringify(state.items));
+//         },
+//         changeQuantity(state, action) {
+//             const { productId, quantity } = action.payload;
+//             const indexProductId = (state.items).findIndex(item => item.productId === productId)
+//             if (quantity > 0) {
+//                 state.items[indexProductId].quantity = quantity;
+//             } else {
+//                 // delete state.items[indexProductId];
+//                 state.items = (state.items).filter(item => item.productId !== productId)
+//             }
+//         },
+
+//         toggleStatusTab(state) {
+//             if (state.stateTab === false) {
+//                 state.stateTab = true;
+//             } else {
+//                 state.stateTab = false;
+//             }
+//         }
+//     }
+// })
+// export const { addToCart, changeQuantity, toggleStatusTab } = cartSlice.actions;
+// export default cartSlice.reducer;
